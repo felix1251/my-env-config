5d84ed57533cef08cd203eddf2ce9c7b45912abc
felix1251 <domhack123@gmail.com>
2024-03-20 11:28:27 +0800

    setup lsp

--- lua/felix/core/keymaps.lua
+++ lua/felix/core/keymaps.lua
@@ -14,3 +14,4 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<laader>fh', builtin.help_tags, {})
+
