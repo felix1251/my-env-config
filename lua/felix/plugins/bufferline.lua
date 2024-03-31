5ee01e38b7eadb6448786ebaf2f82d22465578b6
felix1251 <domhack123@gmail.com>
2024-04-01 04:16:37 +0800

    update: bufferline use default bg

--- lua/felix/plugins/bufferline.lua
+++ lua/felix/plugins/bufferline.lua
@@ -15,46 +15,4 @@ require("bufferline").setup({
     show_close_icon = false,
     show_tab_indicators = false,
   },
-  highlights = {
-    fill = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "StatusLineNC" },
-    },
-    background = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "StatusLine" },
-    },
-    buffer_visible = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-    buffer_selected = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-    separator = {
-      fg = { attribute = "bg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "StatusLine" },
-    },
-    separator_selected = {
-      fg = { attribute = "fg", highlight = "Special" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-    separator_visible = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "StatusLineNC" },
-    },
-    close_button = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "StatusLine" },
-    },
-    close_button_selected = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-    close_button_visible = {
-      fg = { attribute = "fg", highlight = "Normal" },
-      bg = { attribute = "bg", highlight = "Normal" },
-    },
-  },
 })
