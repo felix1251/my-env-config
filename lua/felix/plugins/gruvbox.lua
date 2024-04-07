50296979285cc02bcbfb62fa848c2d83cfd5463a
felix1251 <domhack123@gmail.com>
2024-04-07 11:57:17 +0800

    enable gruvbox tranparent

--- lua/felix/plugins/gruvbox.lua
+++ lua/felix/plugins/gruvbox.lua
@@ -22,7 +22,7 @@ require("gruvbox").setup({
     SignColumn = { bg = "NONE" }
   },
   dim_inactive = false,
-  transparent_mode = false,
+  transparent_mode = true,
 })
 
 vim.cmd([[colorscheme gruvbox]])
