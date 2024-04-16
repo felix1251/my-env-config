0bef60eac40f99f88dbeac64e937fea9909eb25b
felix1251 <domhack123@gmail.com>
2024-04-17 07:02:20 +0800

    update tabnine config

--- lua/felix/plugins/tabnine.lua
+++ lua/felix/plugins/tabnine.lua
@@ -1,6 +1,6 @@
 require('tabnine').setup({
   disable_auto_comment = true,
-  accept_keymap = "<m-q>",
+  accept_keymap = "<M-q>",
   dismiss_keymap = "<C-]>",
   debounce_ms = 800,
   suggestion_color = { gui = "#808080", cterm = 244 },
